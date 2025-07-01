86ed08040d09e33ed3d7f5694e1536dbbdef5b8b
Tamilselvan G <tamilselvan9675@gmail.com>
2025-07-01 16:02:28 +0530

    Task Update

--- src/context/ThemeContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export interface ThemeContextType {
-  theme: 'light' | 'dark';
-  setTheme: (theme: 'light' | 'dark') => void;
-}
-
-export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
